1e04dde94f85d6c8037d911b7b75655e2b8dd6de
fcona <fcona@elements-ic.com>
2023-07-31 09:07:00 +0200

    Increased output buffer size

--- er4commlib_global.h
+++ er4commlib_global.h
@@ -85,7 +85,7 @@
  * When calling method readData data corresponding to at most #ER4CL_DATA_ARRAY_SIZE samples is returned.
  * When calling method readData provide an array of float with at least #ER4CL_DATA_ARRAY_SIZE items.
  */
-#define ER4CL_DATA_ARRAY_SIZE (0x40000)
+#define ER4CL_DATA_ARRAY_SIZE (0x400000)
 
 
 /********************\

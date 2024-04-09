cb8942fee4fac4d3340df028cf5ddd7813166af7
Luca Rossi <lucarossi147@gmail.com>
2024-04-09 15:22:39 +0200

    removed gpl header

--- devices/e1/messagedispatcher_e1light.h
+++ devices/e1/messagedispatcher_e1light.h
@@ -1,19 +1,3 @@
-//  Copyright (C) 2022 Benedetta Capozucchi
-//
-//  This file is part of EDR4.
-//
-//  EDR4 is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  EDR4 is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with EDR4.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef MESSAGEDISPATCHER_E1LIGHT_H
 #define MESSAGEDISPATCHER_E1LIGHT_H
 
--- er4commlib_global.h
+++ er4commlib_global.h
@@ -1,20 +1,3 @@
-//  Copyright (C) 2021-2024 Filippo Cona
-//
-//  This file is part of EDR4.
-//
-//  EDR4 is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  EDR4 is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with EDR4.  If not, see <http://www.gnu.org/licenses/>.
-
 /*! \file er4commlib_global.h
  * \brief Defines global macros and typedefs.
  * \note \a ER4COMMLIB_LIBRARY should be defined only during the library compilation.
